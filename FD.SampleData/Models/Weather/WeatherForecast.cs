3df1d3723b59b83c7cc7423b4ceb94262c18ed64
Francisco Araujo <franciscoa@mcrinc.com>
2021-07-04 17:27:51 -0400

    Add Id column to WeatherForecast model

--- FD.SampleData/Models/Weather/WeatherForecast.cs
+++ FD.SampleData/Models/Weather/WeatherForecast.cs
@@ -5,6 +5,7 @@ namespace FD.SampleData.Models.Weather
 {
     public class WeatherForecast
     {
+        public int Id { get; set; }
         public DateTime Date { get; set; }
         public int TemperatureC { get; set; }
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
