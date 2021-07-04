798dc44d2302547fbe0d091c6b4d07aaa1425be1
Francisco Araujo <franciscoa@mcrinc.com>
2021-07-04 17:11:33 -0400

    Add generation of report types and reporttypes model to dbcontext

--- FD.SampleData/Contexts/WeatherForecastDbContext.cs
+++ FD.SampleData/Contexts/WeatherForecastDbContext.cs
@@ -1,6 +1,5 @@
-﻿using FD.SampleData.Data;
-using FD.SampleData.Data.Generators;
-using FD.SampleData.Models;
+﻿using FD.SampleData.Data.Generators;
+using FD.SampleData.Models.Weather;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,6 +9,7 @@ namespace FD.SampleData.Contexts
 {
     public class WeatherForecastDbContext : BaseDbContext
     {
+        public DbSet<ReportType> ReportTypes { get; set; }
         public DbSet<WeatherForecast> WeatherForecasts { get; set; }
 
         public WeatherForecastDbContext()
@@ -27,8 +27,10 @@ namespace FD.SampleData.Contexts
         /// <returns></returns>
         public override async Task Seed(int? seedSize)
         {
+            List<ReportType> reportTypes = await WeatherForecastGenerator.GenerateReportTypes();
+
             // generates weather forecasts 
-            List<WeatherForecast> forecasts = await WeatherForecastGenerator.GenerateForecast(DateTime.Today, seedSize);
+            List<WeatherForecast> forecasts = await WeatherForecastGenerator.GenerateForecasts(DateTime.Today, reportTypes, seedSize);
             await AddRangeAsync(forecasts);
             await SaveChangesAsync();
         }
