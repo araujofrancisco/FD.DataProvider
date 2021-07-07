ac8a3ba022566176304b6cade1a41d526d51ded7
Francisco Araujo <franciscoa@mcrinc.com>
2021-07-06 20:31:56 -0400

    Add additional names, roles and report types

--- FD.SampleData/Data/Generics.cs
+++ FD.SampleData/Data/Generics.cs
@@ -5,12 +5,17 @@
         // TODO: use https://github.com/smashew/NameDatabases to generate a bigger set of names and surnames
         public static readonly string[] FirstNames = new[]
         {
-            "Anna", "Edward", "John", "Paul", "Dawn", "Kyle", "Sheldon", "Douglas", "Martin", "Luke", "Phil", "Jane", "Joseph", "Peter", "Franklin"
+            "Anna", "Edward", "John", "Paul", "Dawn", "Kyle", "Sheldon", "Douglas", "Martin", "Luke", "Phil", "Jane", "Joseph", "Peter", "Franklin",
+            "Aaron", "Abby", "Abdul", "Abraham", "Ambrose", "Amy", "Arnold", "Arthur", "Brandy", "Brian", "Brigid", "Cathy", "Celine", "Chad",
+            "Colin", "Corey", "Constance", "Debiie", "Dillon", "Diana", "Dixie", "Dolly", "Donald", "Dominique", "Erika", "Ellen", "Elliot",
+            "Farrah", "Evita", "Fanny", "Fay", "Gabriela", "Fred", "Ivonne", "Jackeline", "Jenny", "Johanna", "Jodie", "Kaitlyn", "Kara", "Kelly"
         };
 
         public static readonly string[] LastNames = new[]
         {
-            "Doe", "Perry", "Raussman", "Ford", "Thompson", "Smith", "Simpson", "Dunphy", "Brown", "Blake", "Cooper", "Jackson"
+            "Doe", "Perry", "Raussman", "Ford", "Thompson", "Smith", "Simpson", "Dunphy", "Brown", "Blake", "Cooper", "Jackson", "Acre", "Adam",
+            "Acosta", "Adler", "Aguilar", "Albany", "Allen", "Badder", "Bafford", "Beiswanger", "Bierley", "Boehler", "Borowicz", "Brabson", "Brader",
+            "Cartier", "Caterino", "Cialella", "Delashmit", "Dermo", "Ertzbischoff", "Eytcheson", "Fiotodimitrak", "Garrington", "Harvilchuck", "Harwick"
         };
 
         public static readonly string[] EmailProvider = new[]
@@ -20,7 +25,7 @@
 
         public static readonly string[] Roles = new[]
         {
-            "Administrator", "PowerUser", "Reports", "Import", "Export", "User"
+            "Administrator", "PowerUser", "Reports", "Import", "Export", "User", "Guest"
         };
 
         public static readonly string[] WeatherTypes = new[]
@@ -31,7 +36,7 @@
 
         public static readonly string[] ReportTypes = new[]
         {
-            "Express", "Daily", "Weekly", "None"
+            "Express", "Daily", "Weekly", "None", "Monthly"
         };
     }
 }
