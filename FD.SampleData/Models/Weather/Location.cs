934a15324c9e02e3e53343e71bf002e3b37d5582
Francisco Araujo <franciscoa@mcrinc.com>
2021-07-26 16:15:55 -0400

    Add location model to weatherforecasts

---
+++ FD.SampleData/Models/Weather/Location.cs
@@ -0,0 +1,10 @@
+﻿namespace FD.SampleData.Models.Weather
+{
+    public class Location
+    {
+        public int ID { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
